320197aa989e08d3c947cd87a73026f38e72795a
afsalmarattil <afsal.marattil@robok.ai>
2024-05-27 03:37:59 +0100

    feat(api): apis for search and similarity

---
+++ services/api/company-search/app/api/company/router.py
@@ -0,0 +1,93 @@
+from typing import List, Optional
+
+from elasticsearch import AsyncElasticsearch
+from fastapi import APIRouter, HTTPException, Query
+from pydantic import BaseModel, HttpUrl
+
+es = AsyncElasticsearch(hosts=["http://localhost:9200"])
+router = APIRouter()
+
+
+class Company(BaseModel):
+    id: str
+    linkedin_url: Optional[str] = None
+    company_name: Optional[str] = None
+    industry: Optional[str] = None
+    website: Optional[str] = None
+    tagline: Optional[str] = None
+    about: Optional[str] = None
+    year_founded: Optional[int] = None
+    locality: Optional[str] = None
+    country: Optional[str] = None
+    current_employee_estimate: Optional[int] = None
+    keywords: Optional[str] = None
+
+
+@router.get("/", response_model=dict)
+async def get_companies(
+    page: int = Query(1, ge=1, description="Page number, starting from 1"),
+    page_size: int = Query(10, ge=1, le=100, description="Number of items per page"),
+):
+    skip = (page - 1) * page_size
+    response = await es.search(
+        index="companies", body={"query": {"match_all": {}}}, from_=skip, size=page_size
+    )
+    total_hits = response["hits"]["total"]["value"]
+    companies = [
+        {**hit["_source"], "id": hit["_id"]} for hit in response["hits"]["hits"]
+    ]
+    return {
+        "total": total_hits,
+        "page": page,
+        "page_size": page_size,
+        "total_pages": (total_hits + page_size - 1) // page_size,
+        "results": companies,
+    }
+
+
+@router.get("/{company_id}", response_model=Company)
+async def get_company(company_id: str):
+    try:
+        response = await es.get(index="companies", id=company_id)
+        return {**response["_source"], "id": response["_id"]}
+    except Exception as e:
+        raise HTTPException(status_code=404, detail="Company not found")
+
+
+@router.get("/search/{name}", response_model=List[Company])
+async def search_company(name: str):
+    query = {"query": {"match": {"company_name": name}}}
+    response = await es.search(index="companies", body=query, size=100)
+    return [{**hit["_source"], "id": hit["_id"]} for hit in response["hits"]["hits"]]
+
+
+@router.get("/similar/{company_id}", response_model=list[Company])
+async def similar_companies(company_id: str):
+    try:
+        company = await es.get(index="companies", id=company_id)
+        source = company["_source"]
+        industry = source["industry"]
+        keywords = source["keywords"]
+
+        query = {
+            "query": {
+                "bool": {
+                    "should": [
+                        {"match": {"industry": industry}},
+                        {"match": {"keywords": keywords}},
+                    ]
+                }
+            }
+        }
+
+        response = await es.search(index="companies", body=query, size=3)
+        return [
+            {**hit["_source"], "id": hit["_id"]} for hit in response["hits"]["hits"]
+        ]
+    except Exception as e:
+        raise HTTPException(status_code=404, detail="Unable to find similar companies")
+
+
+@router.on_event("shutdown")
+async def shutdown_event():
+    await es.close()
---
+++ services/api/company-search/app/api/data/csv_upload_router.py
@@ -0,0 +1,72 @@
+import io
+
+import pandas as pd
+from elasticsearch import AsyncElasticsearch, helpers
+from fastapi import APIRouter, File, HTTPException, UploadFile
+from fastapi.responses import JSONResponse
+
+router = APIRouter()
+es = AsyncElasticsearch(hosts=["http://localhost:9200"])
+index_name = "companies"
+
+
+async def create_index():
+    if await es.indices.exists(index=index_name):
+        await es.indices.delete(index=index_name)
+    # Recreate the index with the necessary mappings
+    mapping = {
+        "mappings": {
+            "properties": {
+                "linkedin_url": {"type": "keyword"},
+                "company_name": {"type": "text"},
+                "industry": {"type": "text"},
+                "website": {"type": "keyword"},
+                "tagline": {"type": "text"},
+                "about": {"type": "text"},
+                "year_founded": {"type": "float"},
+                "locality": {"type": "text"},
+                "country": {"type": "keyword"},
+                "current_employee_estimate": {"type": "integer"},
+                "keywords": {"type": "text"},
+            }
+        }
+    }
+    await es.indices.create(index=index_name, body=mapping)
+
+
+@router.post("/upload-csv/")
+async def upload_csv(file: UploadFile = File(...)):
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(
+            status_code=400, detail="Invalid file type. Only CSV files are accepted."
+        )
+
+    content = await file.read()
+    try:
+        # Recreate the index to ensure a fresh start
+        await create_index()
+        df = pd.read_csv(io.StringIO(content.decode("utf-8")))
+        await helpers.async_bulk(es, doc_generator(df))
+        return JSONResponse(
+            content={"message": "File successfully uploaded and indexed"},
+            status_code=200,
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+async def doc_generator(df):
+    for _, row in df.iterrows():
+        yield {
+            "_index": index_name,
+            "_source": {
+                key: (row[key] if pd.notna(row[key]) and row[key] != "" else None)
+                for key in row.index
+                if key != "id"  # Exclude the 'id' column from the source
+            },
+        }
+
+
+@router.on_event("shutdown")
+async def shutdown_event():
+    await es.close()
